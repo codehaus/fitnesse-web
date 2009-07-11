e92119f53d339f6721bf8aa887b4bbd3be1dc4ba
krzycho <krzycho@370a8ec6-cf61-0410-b8bb-af44a55b7162>
2009-07-11 21:19:56 +0000

    Minor changes.

--- examples/standalone/src/main/java/org/codehaus/fitnesseweb/example/standalone/MyControlFixture.java
+++ examples/standalone/src/main/java/org/codehaus/fitnesseweb/example/standalone/MyControlFixture.java
@@ -4,6 +4,6 @@ import org.codehaus.fitnesseweb.fixture.SeleniumControlFixture;
 
 public class MyControlFixture extends SeleniumControlFixture {
     public MyControlFixture() {
-        applicationRoot = "http://www.onet.pl";
+        applicationRoot = "http://localhost:8025/MyProject.SimpleSuite.FirstTest";
     }
 }
